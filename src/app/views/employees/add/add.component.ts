c15d7ced004f9ef1c0270b982a4bd282798907a3
Umesh Mandal <umesh.mandal619@gmail.com>
2018-06-20 09:57:07 +0545

    Add files via upload

--- src/app/views/employees/add/add.component.ts
+++ src/app/views/employees/add/add.component.ts
@@ -5,6 +5,7 @@ import { EmpService } from '../shared/emp.service';
 @Component({
   selector: 'app-add',
   templateUrl: 'add.component.html',
+  styleUrls: ['add.component.css']
 })
 export class AddComponent implements OnInit {
 
