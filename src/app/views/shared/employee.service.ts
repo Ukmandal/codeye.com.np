154358498923d211c63a61867394e260fce784da
Umesh Mandal <umesh.mandal619@gmail.com>
2018-06-28 15:18:28 +0545

    Add files via upload

--- src/app/views/shared/employee.service.ts
+++ src/app/views/shared/employee.service.ts
@@ -2,21 +2,17 @@ import { Injectable } from "@angular/core";
 import { ADMIN_DB } from "./mock-list";
 import { IEmployee } from "./interface";
 import 'rxjs/add/observable/of';
-import { Observable } from 'rxjs';
-// import { of } from "rxjs/observable/of";
-// import { of } from 'rxjs';
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class EmployeeService {
-
-    constructor() { }
 
     getEmployees(): Observable<IEmployee[]> {
         return Observable.of(ADMIN_DB);
     }
 
     //get single data from database to component
-    getEmployee(id: number): Observable<IEmployee> {
+    getEmployee(id): Observable<IEmployee> {
         return Observable.of(ADMIN_DB.find(x => x.id === id));
 
     }
@@ -25,6 +21,6 @@ export class EmployeeService {
         let max = Math.max(...ADMIN_DB.map(x => x.id));
         user.id = max + 1;
         ADMIN_DB.push(user);
-        return ADMIN_DB;
+        return user;
     }
 }
--- src/app/views/shared/interface.ts
+++ src/app/views/shared/interface.ts
@@ -1,7 +1,9 @@
 
 export interface IEmployee {
     id: number;
+    imageUrl: string;
     email: string;
     userName: string;
     password: string;
+    repeatPassword: string;
 }
