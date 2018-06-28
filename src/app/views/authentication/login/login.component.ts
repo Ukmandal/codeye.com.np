f63d13c638f9e5ad2654eb648786f5258f1a4212
Umesh Mandal <umesh.mandal619@gmail.com>
2018-06-28 15:10:32 +0545

    Add files via upload

--- src/app/views/authentication/login/login.component.ts
+++ src/app/views/authentication/login/login.component.ts
@@ -2,32 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   templateUrl: 'login.component.html',
   styles: [`
     .form-control-password {
       position: relative;
-      top: -29px;
-      left: -4px;
       cursor: pointer;
+      margin-top: -30px;
+      margin-right: 10px;
   },
   `],
 })
 export class LoginComponent implements OnInit {
   password: boolean = false;
   loginForm: FormGroup;
-  islogoutSuceeded: boolean;
   invalidCredentialMsg: string;
 
 
   constructor(
     private authService: AuthService,
     private router: Router,
+    public snackBar: MatSnackBar,
     private fb: FormBuilder,
   ) { }
 
   ngOnInit() {
+    this.authService.onLoginSuccess.next();
     this.loginForm = this.fb.group({
       email: [null, [Validators.required, Validators.email]],
       password: [null, [Validators.required, Validators.minLength(4), Validators.maxLength(30)]],
@@ -45,9 +47,16 @@ export class LoginComponent implements OnInit {
       .subscribe(
         authenticated => {
           if (authenticated) {
+            this.authService.onLoginSuccess.next('i am admin')
             let url = this.authService.getRedirectUrl();
             console.log('Redirected Url:' + url);
+            console.log('loggedIn Successfully!!');
             this.router.navigate([url]);
+            this.snackBar.open('loggedIn Successfully!','Got it!', {
+              duration: 3000,
+              verticalPosition: 'bottom',
+              horizontalPosition: 'center',
+          });
           } else {
             this.invalidCredentialMsg = 'Invalid Credentials. Try Again.';
           }
