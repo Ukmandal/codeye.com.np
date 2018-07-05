695e774cb44e340ea42333d7eede335ae1a266fc
Umesh Mandal <umesh.mandal619@gmail.com>
2018-07-05 16:22:43 +0545

    Add files via upload

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,23 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { navItems } from './../../_nav';
+import { AuthService } from '../../views/authentication/services/auth.service';
+import { IEmployee } from '../../views/shared/interface';
+import { EmployeeService } from '../../views/shared/employee.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './default-layout.component.html'
 })
-export class DefaultLayoutComponent {
+export class DefaultLayoutComponent implements OnInit {
   public navItems = navItems;
   public sidebarMinimized = true;
   private changes: MutationObserver;
   public element: HTMLElement = document.body;
-  constructor() {
+  isloginSuceeded: boolean = false;
+  // loggedInUser: IEmployee;
 
+  constructor(
+    private authService: AuthService,
+  ) {
     this.changes = new MutationObserver(() => {
       this.sidebarMinimized = document.body.classList.contains('sidebar-minimized')
     });
 
     this.changes.observe(<Element>this.element, {
       attributes: true
+    });
+  }
+
+  ngOnInit() {
+    // this.loggedInUser = this.authService.getLoggedInUser();
+  }
+
+  ngAfterViewInit() {
+    this.authService.onLoginSuccess.subscribe(x => {
+      this.isloginSuceeded = x !== undefined;
     });
   }
 }
