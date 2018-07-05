d11c15a687161bb8bed0ac53ff243d609cae50ea
Umesh Mandal <umesh.mandal619@gmail.com>
2018-07-05 16:24:03 +0545

    Add files via upload

--- src/app/views/customer/customer-list.component.ts
+++ src/app/views/customer/customer-list.component.ts
@@ -3,30 +3,36 @@ import { Router, ActivatedRoute, ParamMap } from "@angular/router";
 import 'rxjs/add/observable/range';
 import { IEmployee } from '../shared/interface';
 import { EmployeeService } from '../shared/employee.service';
+import { AuthService } from '../authentication/services/auth.service';
+import { ADMIN_DB } from '../shared/mock-list';
 
 @Component({
     selector: 'customer-list',
     templateUrl: 'customer-list.component.html',
 })
 export class CustomerListComponent implements OnInit {
-
+//
+displayedColumns: string[] = ['id', 'email', 'password', 'repeatPassword', 'userName'];
+  dataSource = ADMIN_DB;
+//
     employees: IEmployee[];
     selectedId: number;
     imageWidth: number = 60;
     showImage: boolean = true;
+    loggedInUser: IEmployee;
 
     constructor(
         public employeeService: EmployeeService,
         private router: Router,
-        private route: ActivatedRoute,
+        private authService: AuthService,
     ) { }
 
     ngOnInit() {
-        // this.route.paramMap.subscribe((params: ParamMap) => {
-        //     let id = parseInt(params.get('id'));
-        //     this.selectedId = id;
         this.employeeService.getEmployees()
-            .subscribe(employees => this.employees = employees);
+            .subscribe(employees => {
+                this.employees = employees;
+                this.loggedInUser = this.authService.getLoggedInUser();
+            });
     }
 
     toggleImage(): void {
